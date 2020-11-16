6a833c4467810ea89861c961b22ab2d89daa7940
Renato Andrade <renatorobot@outlook.com.br>
2020-11-16 11:41:15 -0300

    Navegação inferior(BottomNavigationBar)

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inicio.dart';
+import 'package:youtube/telas/Inscricao.dart';
+import 'package:youtube/telas/Blibioteca.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +10,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Bliblioteca()
+    ];
+
     return Scaffold(
         appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.grey),
@@ -19,8 +34,34 @@ class _HomeState extends State<Home> {
             IconButton(icon: Icon(Icons.account_circle), onPressed: (){}),
           ],
           ),
-          body: Container(
+          body: telas[_indiceAtual],
 
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _indiceAtual,
+            onTap: (indice){
+              setState(() {
+                _indiceAtual  = indice;
+              });
+            },
+            
+            type: BottomNavigationBarType.fixed,
+            fixedColor: Colors.red,
+
+            items: [
+
+              BottomNavigationBarItem(
+                title: Text("Início"), icon: Icon(Icons.home)
+                ),
+              BottomNavigationBarItem(
+                title: Text("Em alta"), icon: Icon(Icons.whatshot)
+                ),
+              BottomNavigationBarItem(
+                title: Text("Inscrições"), icon: Icon(Icons.subscriptions)),
+              BottomNavigationBarItem(
+                title: Text("Bliblioteca"), icon: Icon(Icons.folder)
+                ),
+              
+            ],
           ),
     );
   }
---
+++ lib/telas/Blibioteca.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Bliblioteca extends StatefulWidget {
+  @override
+  _BlibliotecaState createState() => _BlibliotecaState();
+}
+
+class _BlibliotecaState extends State<Bliblioteca> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("bliblioteca"),
+      ),
+    );
+  }
+}
